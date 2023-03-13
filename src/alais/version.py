d7cbe71622c80e79fca86ce734f2fd4e679fb024
ctomkow <ctomkow@gmail.com>
2023-03-12 22:43:41 -0700

    typos

--- src/alais/version.py
+++ src/alais/version.py
@@ -1,4 +1,12 @@
-# This project uses 0ver versioning. See https://0ver.org/ for more information.
-# 0.0.x release includes [docs, tests, bug fix, features, etc.]
-# 0.x.0 release includes [breaking changes, removal of features, etc.]
-__version__ = '0.1.1.dev1'
+# This project uses semver-like version.
+#
+# pre 1.0.0 release
+# 0.0.x includes [docs, tests, bug fixes, CI/CD, performance improvements]
+# 0.x.0 includes [features, any breaking changes, removal of features]
+# post 1.0.0 release
+# 1.0.x includes [docs, tests, bug fixes, CI/CD, performance improvements]
+# 1.x.0 includes [features]
+# x.0.0 includes [breaking changes, removal of features]
+#
+# x.x.x.devN includes [any changes] included in the next public release
+__version__ = '0.2.0.dev1'
