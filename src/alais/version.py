4d6715c7461fc01062e8004a37d6192a1029fd01
ctomkow <ctomkow@gmail.com>
2023-03-12 22:07:04 -0700

    docs

--- src/alais/version.py
+++ src/alais/version.py
@@ -1,1 +1,4 @@
+# This project uses 0ver versioning. See https://0ver.org/ for more information.
+# 0.0.x release includes [docs, tests, bug fix, features, etc.]
+# 0.x.0 release includes [breaking changes, removal of features, etc.]
 __version__ = '0.1.1.dev1'
